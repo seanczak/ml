b1f2b3034474f684efdea76f2ac59850dda518da
Sean Fronczak <sean@1mmcorp.com>
2021-01-30 17:49:41 -0800

    adding music and gdrive functionality

--- ml/_nbdev.py
+++ ml/_nbdev.py
@@ -2,9 +2,23 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"gauth": "99c_data_manage.DLMusic_Data.ipynb",
+         "cred_fpath": "99b_data_manage.gdrive_interact.ipynb",
+         "drive": "99c_data_manage.DLMusic_Data.ipynb",
+         "gtypes": "99c_data_manage.DLMusic_Data.ipynb",
+         "get_root_remote_id": "99c_data_manage.DLMusic_Data.ipynb",
+         "get_folder_id": "99c_data_manage.DLMusic_Data.ipynb",
+         "grab_folder_contents": "99c_data_manage.DLMusic_Data.ipynb",
+         "check_file_exists_remote": "99c_data_manage.DLMusic_Data.ipynb",
+         "get_file_id": "99c_data_manage.DLMusic_Data.ipynb",
+         "download_file": "99c_data_manage.DLMusic_Data.ipynb",
+         "upload_new_file": "99c_data_manage.DLMusic_Data.ipynb",
+         "update_existing_file": "99c_data_manage.DLMusic_Data.ipynb",
+         "sync_file_to_remote": "99c_data_manage.DLMusic_Data.ipynb"}
 
-modules = []
+modules = ["local_repo_path.py",
+           "data_manage/gdrive_interact.py",
+           "repo_management/DLMusic_Data.py"]
 
 doc_url = "https://seanczak.github.io/ml/"
 
